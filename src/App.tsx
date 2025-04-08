c00ed76530fa9b11506e91431b28fd4f456d1f2d
Jorge Matos <jisapjisap@gmail.com>
2025-04-08 14:59:23 +0200

    feat: initialize project with React, TypeScript, and Tailwind CSS
    - Added main application structure with React components.
    - Included Tailwind CSS for styling with a custom configuration.
    - Created utility functions for class name management.
    - Set up TypeScript configuration for the application and Node environment.
    - Added initial assets including SVG and image files.
    - Implemented a basic Header component.
    - Configured Vite as the build tool with React plugin.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">
+        <Header />
+      </h1>
+    </div>
+  )
+}
+
+export default App
