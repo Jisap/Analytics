74dfae2cd57d8e49f6d9bae60ffb5b6a7ded8f16
Jorge Matos <jisapjisap@gmail.com>
2025-04-09 16:06:41 +0200

    feat: enhance Hero component styling and add blurry glow effect; update Tailwind config to include Poppins font

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,30 +6,35 @@ import { heroBanner } from "@/assets"
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <p>
+    <section className="py-1o md:py-16">
+      <div className="container text-center">
+        <div className="max-w-screen-md mx-auto">
+          <p className="text-sm uppercase tracking-wider bg-secondary/50 text-secondary-foreground max-w-max mx-auto
+           px-3 py-1 rounded-full border-t border-blue-500/10 backdrop-blur-3xl mb-6 md:mb-10">
             {heroData.sectionSubtitle}
           </p>
 
-          <h2>
+          <h2 className="text-4xl font-semibold !leading-tight mb-4 md:text-5xl md:mb-5 lg:text-6xl">
             {heroData.sectionTitle}
-            <span>{heroData.decoTitle}</span>
+            <span className="relative isolate ms-4">
+              {heroData.decoTitle}
+              <span className="absolute -z-10 top-2 -left-6 -right-4 bottom-0.5 bg-foreground/5 rounded-full px-8
+              ms-3 border-t border-foreground/20 shadow-[inset_0px_0px_30px_0px] shadow-foreground/20 md:top-3 md:bottom-1 lg:top-4 lg:bottom-2"></span>
+            </span>
           </h2>
 
-          <p>
+          <p className="text-muted-foreground md:text-xl">
             {heroData.sectionText}
           </p>
 
-          <div>
+          <div className="flex justify-center gap-2 mt-6 md:mt-10">
             <Button>Start Free Trial</Button>
             <Button variant="ghost">Watch Demo</Button>
           </div>
         </div>
 
-        <div>
-          <figure>
+        <div className="relative mt-12 max-w-screen-xl mx-auto isolate rounded-xl mt:mt-16"> 
+          <figure className="bg-background/60 border border-slate-800 backdrop-blur-3xl rounded-xl shadow-2xl overflow-hidden">
             <img 
               src={heroBanner} 
               alt="hero-banner" 
@@ -37,6 +42,10 @@ const Hero = () => {
               height={815}  
             />
           </figure>
+
+          {/* Blurry glow effect */}
+          <div className="absolute bg-primary inset-5 blur-[50px] -z-10"></div>
+          <div className="absolute inset-0 bg-primary blur-[200px] scale-y-75 scale-x-125 rounded-full -z-10"></div>
         </div>
       </div>
     </section>
