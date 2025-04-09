5fd9b13e428cffaa6671a315ce3eceda1fa5f86d
Jorge Matos <jisapjisap@gmail.com>
2025-04-09 18:09:24 +0200

    feat: integrate ReactLenis for smooth scrolling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
-
+import { ReactLenis } from 'lenis/react'
 
 
 
 const App = () => {
   return (
-    <div className="relative isolate overflow-hidden">
-      <Header />
+    <ReactLenis root>
+      <div className="relative isolate overflow-hidden">
+        <Header />
 
-      <main>
-        <Hero />
-      </main>
-    </div>
+        <main>
+          <Hero />
+        </main>
+      </div>
+    </ReactLenis>
   )
 }
 
