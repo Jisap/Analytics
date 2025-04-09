fdd3b57156fe7cf1043c158e9180eda53b0493c6
Jorge Matos <jisapjisap@gmail.com>
2025-04-09 16:55:28 +0200

    feat: add motion animations to Hero component and update package.json

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,34 +9,79 @@ import {
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import ReactPlayer from 'react-player/youtube'
 import { CirclePlay } from "lucide-react";
-import React from "react";
+import { motion, Variants } from "motion/react"
 
+const heroChildVariants: Variants = {
+  start: {
+    y: 30,
+    opacity: 0,
+    filter: "blur(5px)",
+  },
+  end: {
+    y: 0,
+    opacity: 1,
+    filter: "blur(0px)",
+    transition: {
+      duration: 0.7,
+      ease: "easeOut",
+    }
+  }
+}
 
+const heroVariant: Variants = {
+  start: {},
+  end: {
+    transition: {
+      staggerChildren: 0.4,
+    
+    }
+  }
+
+}
 
 const Hero = () => {
   return (
     <section className="py-1o md:py-16">
-      <div className="container text-center">
+      <motion.div 
+        variants={heroVariant}
+        initial="start"
+        animate="end"
+        className="container text-center"
+      >
         <div className="max-w-screen-md mx-auto">
-          <p className="text-sm uppercase tracking-wider bg-secondary/50 text-secondary-foreground max-w-max mx-auto
-           px-3 py-1 rounded-full border-t border-blue-500/10 backdrop-blur-3xl mb-6 md:mb-10">
+          <motion.p 
+            variants={heroChildVariants}  
+            initial="start"
+            animate="end"
+            className="text-sm uppercase tracking-wider bg-secondary/50 text-secondary-foreground max-w-max mx-auto
+            px-3 py-1 rounded-full border-t border-blue-500/10 backdrop-blur-3xl mb-6 md:mb-10"
+          >
             {heroData.sectionSubtitle}
-          </p>
+          </motion.p>
 
-          <h2 className="text-4xl font-semibold !leading-tight mb-4 md:text-5xl md:mb-5 lg:text-6xl">
+          <motion.h2 
+            variants={heroChildVariants}
+            className="text-4xl font-semibold !leading-tight mb-4 md:text-5xl md:mb-5 lg:text-6xl"
+          >
             {heroData.sectionTitle}
             <span className="relative isolate ms-4">
               {heroData.decoTitle}
               <span className="absolute -z-10 top-2 -left-6 -right-4 bottom-0.5 bg-foreground/5 rounded-full px-8
               ms-3 border-t border-foreground/20 shadow-[inset_0px_0px_30px_0px] shadow-foreground/20 md:top-3 md:bottom-1 lg:top-4 lg:bottom-2"></span>
             </span>
-          </h2>
+          </motion.h2>
 
-          <p className="text-muted-foreground md:text-xl">
+          <motion.p 
+            variants={heroChildVariants}  
+            className="text-muted-foreground md:text-xl"
+          >
             {heroData.sectionText}
-          </p>
+          </motion.p>
 
-          <div className="flex justify-center gap-2 mt-6 md:mt-10">
+          <motion.div 
+            variants={heroChildVariants}  
+            className="flex justify-center gap-2 mt-6 md:mt-10"
+          >
             <Button>Start Free Trial</Button>
 
             <Dialog>
@@ -60,7 +105,7 @@ const Hero = () => {
                 </AspectRatio>
               </DialogContent>
             </Dialog>
-          </div>
+          </motion.div>
         </div>
 
         <div className="relative mt-12 max-w-screen-xl mx-auto isolate rounded-xl mt:mt-16"> 
@@ -77,7 +122,7 @@ const Hero = () => {
           <div className="absolute bg-primary inset-5 blur-[50px] -z-10"></div>
           <div className="absolute inset-0 bg-primary blur-[200px] scale-y-75 scale-x-125 rounded-full -z-10"></div>
         </div>
-      </div>
+      </motion.div>
     </section>
   )
 }
