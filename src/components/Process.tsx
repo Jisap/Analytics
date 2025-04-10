007679305470742f3769ec58858199a6ca1e7289
Jorge Matos <jisapjisap@gmail.com>
2025-04-10 18:31:38 +0200

    feat: implement Process component with animated sections and dynamic content

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { motion } from "motion/react"
+import * as variants from "@/lib/motionVariants"
+import { processData } from "@/constants"
+
+
+
+
 
 const Process = () => {
   return (
-    <div>Process</div>
+    <section className="section">
+      <div className="container">
+        <div className="section-head">
+                  <motion.p 
+                    variants={variants.fadeInUp}
+                    initial="start"
+                    whileInView="end"
+                    viewport={{ once: true }}
+                    className="section-subtitle"
+                  >
+                    {processData.sectionSubtitle}
+                  </motion.p>
+        
+                  <motion.h2 
+                    variants={variants.fadeInUp}
+                    initial="start"
+                    whileInView="end"
+                    viewport={{ once: true }}
+                    className="section-title"
+                  >
+                    {processData.sectionTitle}
+                  </motion.h2>
+        
+                  <motion.p 
+                    variants={variants.fadeInUp}
+                    initial="start"
+                    whileInView="end"
+                    viewport={{ once: true }}
+                    className="section-text"
+                  >
+                    {processData.sectionText}
+                  </motion.p>
+                </div>
+      </div>
+    </section>
   )
 }
 
