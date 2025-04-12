94a57b8ab1279ca034ad23368a23fe02c2b21d14
Jorge Matos <jisapjisap@gmail.com>
2025-04-12 15:01:15 +0200

    feat: update @radix-ui/react-slot to v1.2.0 and enhance Blog component with animations
    - Updated the version of @radix-ui/react-slot in package.json.
    - Refactored Blog component to include motion animations for section subtitle, title, and text.
    - Added mapping for blog posts with motion support.
    - Introduced a new Badge component for UI consistency.

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,8 +1,70 @@
+import { motion } from "motion/react"
+import * as variants from "@/lib/motionVariants"
+import { blogData } from "@/constants"
+import { 
+  Card, 
+  CardHeader, 
+  CardContent, 
+  CardTitle, 
+  CardFooter 
+} from "./ui/card"
 
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className="section">
+      <div className="container">
+        <div className="section-head">
+          <motion.p
+            variants={variants.fadeInUp}
+            initial="start"
+            whileInView="end"
+            viewport={{ once: true }}
+            className="section-subtitle"
+          >
+            {blogData.sectionSubtitle}
+          </motion.p>
+
+          <motion.h2
+            variants={variants.fadeInUp}
+            initial="start"
+            whileInView="end"
+            viewport={{ once: true }}
+            className="section-title"
+          >
+            {blogData.sectionTitle}
+          </motion.h2>
+
+          <motion.p
+            variants={variants.fadeInUp}
+            initial="start"
+            whileInView="end"
+            viewport={{ once: true }}
+            className="section-text"
+          >
+            {blogData.sectionText}
+          </motion.p>
+
+          <motion.div>
+            {blogData.blogs.map(({
+              imgSrc,
+              badge,
+              title,
+              author: {
+                avatarSrc,
+                authorName,
+                publishDate,
+                readingTime,
+              },
+            }, index) => (
+              <motion.div key={index}>
+
+              </motion.div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+    </div>
   )
 }
 
