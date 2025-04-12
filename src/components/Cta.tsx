06f90295156722f1839ff2ce42438b2bd6734a4e
Jorge Matos <jisapjisap@gmail.com>
2025-04-12 16:38:43 +0200

    feat: restructure Cta component layout for improved styling and responsiveness

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -10,25 +10,29 @@ const Cta = () => {
   return (
     <section className="section">
       <div className="container">
-        <div className="">
-          <motion.h2>
-            {ctaData.text}
-          </motion.h2>
-
-          <motion.div>
-            <Button>Free Trial</Button>
-            <Button
-              variant="outline"
-              className=""
+        <motion.div className="bg-primary rounded-xl border-t border-primary-foreground/30 overflow-hidden grid grid-cols-1 lg:grid-cols-[1fr,0.7fr] lg:items-center">
+          <div className="p-8 md:p-16 xl:p-20">
+            <motion.h2
+              className="text-[26px] leading-tight font-semibold mb-6 capitalize sm:text-[34px] md:text-[40px] lg:text-[46px] lg:mb-10"
             >
-              Pricing & Plans
-            </Button>
-          </motion.div>
-        </div>
+              {ctaData.text}
+            </motion.h2>
 
-        <motion.figure>
-          <img src={ctaBanner} alt="" />
-        </motion.figure>
+            <motion.div>
+              <Button>Free Trial</Button>
+              <Button
+                variant="outline"
+                className=""
+              >
+                Pricing & Plans
+              </Button>
+            </motion.div>
+          </div>
+
+          <motion.figure>
+            <img src={ctaBanner} alt="" />
+          </motion.figure>
+        </motion.div>
       </div>
 
     </section>
