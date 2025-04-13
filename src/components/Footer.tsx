fb6ce96b86f2e85bc3343ed8b6b1eb0010d88fa6
Jorge Matos <jisapjisap@gmail.com>
2025-04-13 11:48:26 +0200

    feat: enhance Footer component layout with grid structure and improved styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,21 +7,21 @@ import { footerData } from "@/constants"
 
 const Footer = () => {
   return (
-    <footer className="section">
-      <div className="container">
-        <div>
+    <footer className="section !pb-0">
+      <div className="container ">
+        <div className="grid grid-cols-1 gap-x-2 gap-y-10 lg:grid-cols-4">
           <Logo />
 
-          <div>
+          <div className="grid grid-cols-2 gap-x-2 gap-y-8 text-sm sm:grid-cols-4 lg:col-span-3">
             {footerData.links.map(({title, items}, index) => (
               <ul key={index}>
-                <p>
+                <p className="mb-4">
                   {title}
                 </p>
 
                 {items.map(({href, label}, index) => (
-                  <li key={index}>
-                    <a href={href}>
+                  <li key={index} className="text-muted-foreground">
+                    <a href={href} className="inline-block py-1 transition-colors hover:text-primary">
                       {label}
                     </a>
                   </li>
